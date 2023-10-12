70649c53e444088045347120be5aae4f563f1105
kdrbipul <kdrbipul@gmail.com>
2023-10-12 09:11:31 +0600

    first commit

---
+++ functions.php
@@ -0,0 +1,8 @@
+<?php
+
+/*
+* My Theme Functions
+*/
+
+//Theme title
+add_theme_support('title-tag');
---
+++ index.php
@@ -0,0 +1,23 @@
+<?php 
+/*
+ * This template for displaying for header 
+ */
+?>
+<!DOCTYPE html>
+<html lang="<?php language_attributes(); ?>" class="no-js">
+<head>
+    <meta charset="<?php bloginfo('charset') ?>" >
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php wp_head();?>
+</head <?php body_class();?> >
+<body>
+    
+
+
+
+    <?php wp_footer();?>
+</body>
+</html>
+
