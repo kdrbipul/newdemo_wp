4800975aeefcc9f4437bb2aa95caec82329b5758
kdrbipul <kdrbipul@gmail.com>
2023-10-12 15:40:03 +0600

    first commit

--- functions.php
+++ functions.php
@@ -6,3 +6,11 @@
 
 //Theme title
 add_theme_support('title-tag');
+
+
+//Theme css and js file calling
+
+function b_css_js_file_calling(){
+    wp_enqueue_style( 'my-style', get_stylesheet_uri());
+}
+add_action('wp_enqueue_scripts','b_css_js_file_calling');
--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <meta charset="<?php bloginfo('charset') ?>" >
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>New Theme</title>
     <?php wp_head();?>
 </head <?php body_class();?> >
 <body>
