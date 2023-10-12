61ec7b6e1f2e0b1988b489151d48fcac1ef9a7ba
kdrbipul <kdrbipul@gmail.com>
2023-10-12 16:21:49 +0600

    last commit

--- functions.php
+++ functions.php
@@ -12,5 +12,17 @@ add_theme_support('title-tag');
 
 function b_css_js_file_calling(){
     wp_enqueue_style( 'my-style', get_stylesheet_uri());
+
+    // bootstrap location add
+    wp_register_style('bootstrap', get_template_directory_uri().'/css/bootstrap.css', array(), '4.0.0', 'all');
+    wp_register_style('custom', get_template_directory_uri().'/css/custom.css', array(), '1.0.0', 'all');
+    wp_enqueue_style('bootstrap');
+    wp_enqueue_style('custom');
+
+
+    // jquery location add
+    wp_enqueue_script('jquery');
+    wp_enqueue_script('bootstrap', get_template_directory_uri().'/js/bootstrap.js', array(), '4.0.0', true);
+    wp_enqueue_script('main', get_template_directory_uri().'/js/main.js', array(), '1.0.0', true);
 }
 add_action('wp_enqueue_scripts','b_css_js_file_calling');
--- index.php
+++ index.php
@@ -11,8 +11,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>New Theme</title>
     <?php wp_head();?>
-</head <?php body_class();?> >
-<body>
+</head>
+<body <?php body_class();?> >
+<div id="header_are"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+                <img src="<?php echo get_template_directory_uri();?>./images/logo.jpg" alt="">
+            </div>
+            <div class="col-md-9"></div>
+        </div>
+    </div>
+</div>
     
 
 
